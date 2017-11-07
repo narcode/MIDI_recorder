34edaf939be22578ab11482990e76389efcc2e27
Rob van Loon <borrob@me.com>
2017-11-07 19:53:09 +0100

    Cleanup

--- recorder.py
+++ recorder.py
@@ -1,15 +1,8 @@
 import time
 import rtmidi
-#import from parrentdir
-import sys
-import os
-import inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+
 from CK_rec.setup import Setup
 from CK_rec.rec_classes import CK_rec
-
 
 # Start the Device
 codeK = Setup()
@@ -21,7 +14,6 @@ print('your note on id is: ', on_id)
 # record
 midiRec = CK_rec(myPort, on_id)
 codeK.set_callback(midiRec)
-
 
 # Loop to program to keep listening for midi input
 try:
